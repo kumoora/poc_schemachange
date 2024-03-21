d2838805d62cc1cd5269069ea6058e413a6cbfe3
kumoora <kumoshiv@gmail.com>
2024-03-21 15:33:17 +0530

    adding column

--- migrations/MDM_POND/CLEANSED/Tables/R__FirstTable.sql
+++ migrations/MDM_POND/CLEANSED/Tables/R__FirstTable.sql
@@ -1,4 +1,6 @@
-create table cleansed.first_table(
+create table if not exists cleansed.first_table(
     first_column varchar,
     second_column varchar
-) 
+) ;
+
+alter table cleansed.first_table add column third_column varchar;
