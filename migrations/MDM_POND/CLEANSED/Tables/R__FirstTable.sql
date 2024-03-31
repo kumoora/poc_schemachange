4d3dc603ed0c4b5e19ac2c2cdf443f84be1dd6d5
kumoora <132431831+kumoora@users.noreply.github.com>
2024-04-01 03:56:35 +0530

    Update R__FirstTable.sql

--- migrations/MDM_POND/CLEANSED/Tables/R__FirstTable.sql
+++ migrations/MDM_POND/CLEANSED/Tables/R__FirstTable.sql
@@ -2,6 +2,6 @@ create or replace table  cleansed.first_table_1(
     first_column12 varchar,
     secondcolumn varchar,
     thirdcolumn varchar,
-    fourthcolumn44 varchar
+    fourthcolumn444 varchar
 ) ;
 
